efccad42d7af7aa76927b1216c4dd7be26d6f7a9
Matthieu Mitrani <same31@users.noreply.github.com>
2020-05-06 11:47:19 +0100

    Handles images only

--- handler.js
+++ handler.js
@@ -12,16 +12,12 @@ const request = require('request');
  * http://localhost:3000/?url=https://github.com
  */
 module.exports.corsProxy = (event, context, callback) => {
-    let params = event.queryStringParameters;
-    let { Host, host, Origin, origin, ...headers } = event.headers;
-
-    console.log(event);
-    console.log(`Got request with params:`, params);
+    const params = event.queryStringParameters;
 
     if (!params.url) {
         const errorResponse = {
             statusCode: 400,
-            body: 'Unable get url from \'url\' query parameter'
+            body: "Unable get url from 'url' query parameter"
         };
 
         callback(null, errorResponse);
@@ -30,40 +26,38 @@ module.exports.corsProxy = (event, context, callback) => {
     }
 
     return new Promise((resolve, reject) => {
-        let originalRequestBody = event.body;
-        request({
-            url: params.url,
-            method: event.httpMethod,
-            timeout: 20000,
-            json: event.httpMethod === 'POST' ? JSON.parse(originalRequestBody) : null,
-            headers,
-        }, (err, originalResponse, body) => {
-            if (err) {
-                console.log(`Got error`, err);
+        request(
+          {
+              url: params.url,
+              method: event.httpMethod,
+              timeout: 20000,
+              encoding: null,
+              Accept: 'image/*'
+          },
+          (err, originalResponse) => {
+              if (err) {
+                  callback(err);
 
-                callback(err);
+                  reject(err);
 
-                reject(err);
+                  return;
+              }
 
-                return;
-            }
+              const proxyBody = originalResponse.body.toString('base64');
 
-            console.log(`Got response from ${params.url} ---> {statusCode: ${originalResponse.statusCode}}`);
-            const proxyBody = originalRequestBody ? JSON.stringify(body) : originalResponse.body;
+              const proxyResponse = {
+                  statusCode: originalResponse.statusCode,
+                  headers: {
+                      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+                      'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+                      'content-type': originalResponse.headers['content-type']
+                  },
+                  isBase64Encoded: true,
+                  body: proxyBody
+              };
 
-            const proxyResponse = {
-                statusCode: originalResponse.statusCode,
-                headers: {
-                    "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-                    "Access-Control-Allow-Credentials" : true, // Required for cookies, authorization headers with HTTPS
-                    "content-type": originalResponse.headers['content-type']
-                },
-                body: proxyBody
-            };
-
-            callback(null, proxyResponse);
-
-            resolve(proxyResponse);
-        });
+              resolve(proxyResponse);
+          }
+        );
     });
 };
