1b4b1b44755a7e59cb5ec40413e470b7793b86e3
Jared K <jaredkotoff@users.noreply.github.com>
2020-09-20 17:54:58 -0700

    Update corsProxy() to be async and use fetch

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-const request = require('request');
+const fetch = require("node-fetch");
 
 /**
  * Use this command to launch the handler from console:
@@ -11,59 +11,65 @@ const request = require('request');
  *
  * http://localhost:3000/?url=https://github.com
  */
-module.exports.corsProxy = (event, context, callback) => {
+module.exports.corsProxy = async (event) => {
+  return new Promise((resolve, reject) => {
     let params = event.queryStringParameters;
     let { Host, host, Origin, origin, ...headers } = event.headers;
+    let proxyResponse = {};
 
     console.log(event);
     console.log(`Got request with params:`, params);
 
     if (!params.url) {
-        const errorResponse = {
-            statusCode: 400,
-            body: 'Unable get url from \'url\' query parameter'
-        };
-
-        callback(null, errorResponse);
-
-        return;
+      const errorResponse = {
+        statusCode: 400,
+        body: "Unable get url from 'url' query parameter",
+      };
+      reject(Error(errorResponse));
+      return;
     }
 
-    return new Promise((resolve, reject) => {
-        let originalRequestBody = event.body;
-        request({
-            url: params.url,
-            method: event.httpMethod,
-            timeout: 20000,
-            json: event.httpMethod === 'POST' ? JSON.parse(originalRequestBody) : null,
-            headers,
-        }, (err, originalResponse, body) => {
-            if (err) {
-                console.log(`Got error`, err);
+    const requestParams = Object.entries(params)
+      .reduce((acc, param) => {
+        if (param[0] !== "url") acc.push(param.join("="));
+        return acc;
+      }, [])
+      .join("&");
 
-                callback(err);
+    const url = `${params.url}${requestParams}`;
+    fetch(url, {
+      method: event.httpMethod,
+      timeout: 20000,
+      body: event.httpMethod === "POST" ? event.body : null,
+      headers,
+    })
+      .then((res) => {
+        console.log(
+          `Got response from ${url} ---> {statusCode: ${res.status}}`,
+        );
 
-                reject(err);
+        proxyResponse = {
+          statusCode: res.status,
+          headers: {
+            "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+            "content-type": res.headers["content-type"],
+          },
+        };
+        return res.text();
+      })
+      .then((body) => {
+        proxyResponse.body = body;
 
-                return;
-            }
+        resolve(proxyResponse);
+      })
+      .catch((err) => {
+        if (err) {
+          console.error(`Got error`, err);
 
-            console.log(`Got response from ${params.url} ---> {statusCode: ${originalResponse.statusCode}}`);
-            const proxyBody = originalRequestBody ? JSON.stringify(body) : originalResponse.body;
-
-            const proxyResponse = {
-                statusCode: originalResponse.statusCode,
-                headers: {
-                    "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-                    "Access-Control-Allow-Credentials" : true, // Required for cookies, authorization headers with HTTPS
-                    "content-type": originalResponse.headers['content-type']
-                },
-                body: proxyBody
-            };
-
-            callback(null, proxyResponse);
-
-            resolve(proxyResponse);
-        });
-    });
+          reject(err);
+          return;
+        }
+      });
+  });
 };
